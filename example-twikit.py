d806a79eb07c5973288d46ea14c0dc01289ac3ce
kabixi000 <kabixi000@gmail.com>
2024-12-12 04:24:34 +0000

    Add: 環境変数用の例ファイルを追加し、認証情報の取得方法を修正

--- example-twikit.py
+++ example-twikit.py
@@ -6,14 +6,10 @@ from twikit import Client
 # ユーザー名とパスワードを環境変数から取得
 dotenv.load_dotenv()
 USERNAME = os.environ.get('TWITTER_USERNAME', 'your_username')
-print('username:', USERNAME)
 PASSWORD = os.environ.get('TWITTER_PASSWORD', 'your_password')
 EMAIL = os.environ.get('TWITTER_EMAIL', 'your_email')
 
-
-# USERNAME = 'example_user'
-# EMAIL = 'email@example.com'
-# PASSWORD = 'password0000'
+print('username:', USERNAME)
 
 # Initialize client
 client = Client('en-US')
@@ -21,7 +17,7 @@ client = Client('en-US')
 async def main():
     # アカウントにログイン
     await client.login(
-        auth_info_1=USERNAME ,
+        auth_info_1=USERNAME,
         auth_info_2=EMAIL,
         password=PASSWORD
     )
